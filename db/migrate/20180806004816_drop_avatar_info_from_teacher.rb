d9f2feb56313b39b755ba62651f1d7e062475d5b
Jessica <jmc617@gmail.com>
2018-08-05 22:17:41 -0400

    pg changes

--- db/migrate/20180806004816_drop_avatar_info_from_teacher.rb
+++ db/migrate/20180806004816_drop_avatar_info_from_teacher.rb
@@ -1,6 +1,6 @@
 class DropAvatarInfoFromTeacher < ActiveRecord::Migration[5.2]
   def change
-    remove_foreign_key :teachers, :avatar
+    
     remove_column :teachers, :avatar_file_name, :string
     remove_column :teachers, :avatar_content_type, :string
     remove_column :teachers, :avatar_file_size, :integer
