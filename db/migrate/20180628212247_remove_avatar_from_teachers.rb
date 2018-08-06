495fa51edc187da09c087e90508de96336a348a3
Jessica <jmc617@gmail.com>
2018-08-05 22:07:55 -0400

    db changes 2

--- db/migrate/20180628212247_remove_avatar_from_teachers.rb
+++ db/migrate/20180628212247_remove_avatar_from_teachers.rb
@@ -1,6 +1,6 @@
 class RemoveAvatarFromTeachers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :teachers, :avatar, :string
+    # remove_column :teachers, :avatar, :string
     add_column :teachers, :attachments, :string
   end
 end
